c60e4412fec11ededfe9429395ffaac3ae275077
Brian Cheng <brian.cheng@ucla.edu>
2020-12-17 02:32:49 -0800

    Add icons to tab bar

--- frontend/App.js
+++ frontend/App.js
@@ -2,12 +2,27 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons, Ionicons, FontAwesome5 } from '@expo/vector-icons';
 import { DiagnosticsTab, DataTab, HomeTab, AmbianceTab, Start } from './homeTabs';
 import SettingsTab from './settingsTab';
 
 function HomeTabs() {
   return (
-    <Tab.Navigator initialRouteName="Home">
+    <Tab.Navigator initialRouteName="Home" screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        if (route.name === "Diagnostics") {
+          return <MaterialCommunityIcons name="hospital" size={size} color={color} />;
+        } else if (route.name === "Data") {
+          return <MaterialCommunityIcons name="waveform" size={size} color={color} />;
+        } else if (route.name === "Home") {
+          return <Ionicons name="ios-globe-outline" size={size} color={color} />;
+        } else if (route.name === "Ambiance") {
+          return <FontAwesome5 name="lightbulb" size={size} color={color} />;
+        } else if (route.name === "Settings") {
+          return <Ionicons name="settings-outline" size={size} color={color} />;
+        }
+      }
+    })}>
       <Tab.Screen name="Diagnostics" component={DiagnosticsTab} />
       <Tab.Screen name="Data" component={DataTab} />
       <Tab.Screen name="Home" component={HomeTab} />

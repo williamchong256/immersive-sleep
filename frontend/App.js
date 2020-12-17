40828ec1754edbcbd30e2a99dc42f2e6a4a95f09
Brian Cheng <brian.cheng@ucla.edu>
2020-12-16 20:39:56 -0800

    Initial tab layout

--- frontend/App.js
+++ frontend/App.js
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { DiagnosticsTab, DataTab, HomeTab, AmbianceTab, SettingsTab, Start } from './homeTabs.js';
 
-export default function App() {
+function HomeTabs() {
   return (
-    <View style={styles.container}>
-      <Text>Sleep thingy holder text</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator initialRouteName="Home">
+      <Tab.Screen name="Diagnostics" component={DiagnosticsTab} />
+      <Tab.Screen name="Data" component={DataTab} />
+      <Tab.Screen name="Home" component={HomeTab} />
+      <Tab.Screen name="Ambiance" component={AmbianceTab} />
+      <Tab.Screen name="Settings" component={SettingsTab} />
+    </Tab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createStackNavigator();
+
+const Tab = createBottomTabNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Start" component={Start} />
+        <Stack.Screen name="Home" component={HomeTabs} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;

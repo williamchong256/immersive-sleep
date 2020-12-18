6cf592ad0e786e96ff2c7127479600e5cd8ef5b8
Brian Cheng <brian.cheng@ucla.edu>
2020-12-17 23:02:09 -0800

    Add comments to frontend code
    Comment tab navigation implementation

--- frontend/App.js
+++ frontend/App.js
@@ -6,10 +6,19 @@ import { MaterialCommunityIcons, Ionicons, FontAwesome5 } from '@expo/vector-ico
 import { DiagnosticsTab, DataTab, HomeTab, AmbianceTab, Start } from './homeTabs';
 import SettingsTab from './settingsTab';
 
+// Implements the bottom tab navigation
+// Tab icons are implemented in the tabBarIcon option in the TabNavigator,
+// but could just as easily be implemented in each individual TabScreen
 function HomeTabs() {
   return (
+    // The route to "Home" is rendered on first load of the navigator
+    // The screenOptions options in the Navigator sets the default options for
+    // each of the child Screens
+    // This function is passed down to each individual Screen, which then passes
+    // its route prop to this function
     <Tab.Navigator initialRouteName="Home" screenOptions={({ route }) => ({
       tabBarIcon: ({ focused, color, size }) => {
+        // Switch statements that assigns icon based on TabScreen name
         switch (route.name) {
           case "Diagnostics":
             return <MaterialCommunityIcons name="hospital" size={size} color={color} />;
@@ -26,6 +35,7 @@ function HomeTabs() {
         }
       }
     })}>
+      {/* The different tabs routes are declared here as Screens */}
       <Tab.Screen name="Diagnostics" component={DiagnosticsTab} />
       <Tab.Screen name="Data" component={DataTab} />
       <Tab.Screen name="Home" component={HomeTab} />
@@ -42,6 +52,13 @@ const Tab = createBottomTabNavigator();
 function App() {
   return (
     <NavigationContainer>
+      {/*
+        The Tab Navigator is nested inside the Stack Navigator so that
+        the "Start" Screen does not display the bottom tab bar when
+        navigated to
+
+        For more information: https://reactnavigation.org/docs/hiding-tabbar-in-screens
+      */}
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Start" component={Start} />
         <Stack.Screen name="Home" component={HomeTabs} options={{ headerShown: false }} />
--- frontend/settingsTab.js
+++ frontend/settingsTab.js
@@ -1,21 +1,31 @@
+// Holds the various components reached from the Settings tab
+
 import * as React from 'react';
 import { Button, View, Text, Switch } from 'react-native';
 import { styles } from './style';
 import { createStackNavigator } from '@react-navigation/stack';
 
+// TODO: Implement Dark Mode at an app level
+
+// The component for the initial landing page for Settings
 function Settings({ navigation }) {
     const [ darkMode, setDarkMode ] = React.useState(false);
 
     return (
-        <View style={[styles.settings, {
-            backgroundColor: darkMode ? '#000' : '#fff',
-        }]}>
+        // Returns a View containing Buttons that navigate to
+        // the various Screens in the SettingsStack
+        <View style={styles.settings}>
             <Button onPress={() => navigation.navigate('Profile')} title='Profile' />
             <Button onPress={() => navigation.navigate('Preferences')} title='Preferences' />
+            {/*
+            To display the Dark Mode toggle as shown in the figma, a Text
+            component and a View component with a Switch component nested
+            inside are further nested inside another View and are given
+            specific styles. These styles can been inspected in the
+            style.js file
+            */}
             <View style={styles.darkMode}>
-                <Text style={[styles.darkModeText, {
-                    color: darkMode ? '#fff' : '#000'
-                }]}>Dark Mode</Text>
+                <Text style={styles.darkModeText}>Dark Mode</Text>
                 <View style={styles.darkModeToggleView}>
                     <Switch onChange={() => setDarkMode(!darkMode)} value={darkMode} />
                 </View>
@@ -25,7 +35,6 @@ function Settings({ navigation }) {
         </View>
     );
 }
-
 
 function Profile() {
     return (
@@ -63,6 +72,7 @@ const SettingsStack = createStackNavigator();
 
 function SettingsTab() {
     return (
+        // Implement a StackNavigator for the various settings
         <SettingsStack.Navigator initialRouteName="Settings">
             <SettingsStack.Screen name="Settings" component={Settings} />
             <SettingsStack.Screen name="Preferences" component={Preferences} />

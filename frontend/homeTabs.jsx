2b783c9b7b6ba22460e92bdf1111a399ad243d54
Brian Cheng <brian.cheng@ucla.edu>
2020-12-25 17:14:26 -0800

    Fix formatting in homeTabs

--- frontend/homeTabs.jsx
+++ frontend/homeTabs.jsx
@@ -3,9 +3,11 @@
 // are navigated to (except for the Settings Screen)
 
 import * as React from 'react';
-import { Button, View, Text, Dimensions } from 'react-native';
-import { styles } from './style';
-import { LineChart } from "react-native-chart-kit";
+import {
+  Button, View, Text, Dimensions,
+} from 'react-native';
+import { LineChart } from 'react-native-chart-kit';
+import styles from './style';
 import { sampleData } from './sampleData.json';
 
 export function Start() {
@@ -17,26 +19,26 @@ export function Start() {
 }
 
 export function DiagnosticsTab() {
-    const daysOfTheWeek = sampleData.map(user => user.key);
-    const heartRateDiagnostics = sampleData.map(user => user.heartRate);
+  const daysOfTheWeek = sampleData.map((user) => user.key);
+  const heartRateDiagnostics = sampleData.map((user) => user.heartRate);
 
-    return (
-        <View style={styles.container}>
-            <Text>Heart Rate Analysis</Text>
-            <LineChart 
-            data = {{labels: daysOfTheWeek, datasets: [{ data: heartRateDiagnostics }]}}
-            height= {200}
-            width = {Dimensions.get("window").width-60}
-            fromZero={true}
-            withShadow={false}
-            chartConfig={{
-                backgroundGradientFrom: "#39B7CD", 
-                backgroundGradientTo: "#39B7CD",
-                color: (opacity=1) => `rgba(0,0,0, ${opacity})`
-            }}
-            />
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text>Heart Rate Analysis</Text>
+      <LineChart
+        data={{ labels: daysOfTheWeek, datasets: [{ data: heartRateDiagnostics }] }}
+        height={200}
+        width={Dimensions.get('window').width - 60}
+        fromZero
+        withShadow={false}
+        chartConfig={{
+          backgroundGradientFrom: '#39B7CD',
+          backgroundGradientTo: '#39B7CD',
+          color: (opacity = 1) => `rgba(0,0,0, ${opacity})`,
+        }}
+      />
+    </View>
+  );
 }
 
 export function HomeTab({ navigation }) {

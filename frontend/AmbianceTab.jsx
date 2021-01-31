b16ec2c390dc73b753faf1be3c10b603e1f34598
Brian Cheng <brian.cheng@ucla.edu>
2021-01-30 16:55:02 -0800

    Refactor Lighting code

--- frontend/AmbianceTab.jsx
+++ frontend/AmbianceTab.jsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import {
-  View, Text, Pressable, Switch,
+  View, Text, Pressable,
 } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Slider from '@react-native-community/slider';
 import { LinearGradient } from 'expo-linear-gradient';
 import styles from './style';
 import {
   CardView, PageTitle, PageView, CardTitle,
 } from './Themes';
+import Lighting from './Lighting';
 
 function Ambiance({ navigation }) {
   return (
@@ -36,74 +36,6 @@ function Music() {
   return (
     <View style={styles.container}>
       <Text>Boilerplate for Music</Text>
-    </View>
-  );
-}
-
-function Lighting() {
-  const [lightOn, setLightOn] = React.useState(false);
-  const [initialSlider, setSlider] = React.useState(0);
-  const [initialLightColor, setLightColor] = React.useState(0);
-  let determineColor = 'No Color';
-  let stringForInitialColor = '#ffd700';
-  let stringForFinalColor = '#00bfff';
-  let noLightIntensity = initialSlider;
-  if ((initialLightColor).toFixed(1) <= 0.5) { determineColor = 'Yellow'; stringForInitialColor = '#ffd700'; stringForFinalColor = '#ffd700'; }
-  if ((initialLightColor).toFixed(1) < 1.0 && (initialLightColor).toFixed(1) > 0.5) { determineColor = 'Blue'; stringForInitialColor = '#00bfff'; stringForFinalColor = '#00bfff'; }
-  if ((initialLightColor).toFixed(1) < 1.5 && (initialLightColor).toFixed(1) >= 1.0) { determineColor = 'Pink'; stringForInitialColor = '#ff69b4'; stringForFinalColor = '#ff69b4'; }
-  if ((initialLightColor).toFixed(1) < 2.0 && (initialLightColor).toFixed(1) >= 1.5) { determineColor = 'Red'; stringForInitialColor = '#b22222'; stringForFinalColor = '#b22222'; }
-  if ((initialLightColor).toFixed(1) < 2.5 && (initialLightColor).toFixed(1) >= 2.0) { determineColor = 'Green'; stringForInitialColor = '#008000'; stringForFinalColor = '#008000'; }
-  if ((initialLightColor).toFixed(1) <= 3.0 && (initialLightColor).toFixed(1) >= 2.5) { determineColor = 'Purple'; stringForInitialColor = '#9400d3'; stringForFinalColor = '#9400d3'; }
-  if (lightOn === false) { determineColor = 'None'; stringForInitialColor = '#000000'; stringForFinalColor = '#000000'; noLightIntensity = 0; }
-  return (
-    <View style={styles.container}>
-      <Text>
-        Turn Light On
-        {'\n'}
-        {'\n'}
-      </Text>
-      <Switch
-        trackColor={{ false: '00FF00', true: 'FFFFFF' }}
-        onValueChange={(value) => setLightOn(value)}
-        value={lightOn}
-        style={{ transform: [{ scaleX: 2 }, { scaleY: 2 }] }}
-      />
-      <Text>
-        {'\n'}
-        {'\n'}
-        {'\n'}
-        {' '}
-        Intensity of Light:
-        {' '}
-        {Math.round(noLightIntensity)}
-      </Text>
-      <Slider
-        minimumValue={0}
-        maximumValue={3}
-        value={initialSlider}
-        onValueChange={(value) => setSlider(value)}
-        style={{ width: 160, height: 50 }}
-        onSlidingComplete={(value) => setSlider(value)}
-        minimumTrackTintColor="#ffd700"
-        maximumTrackTintColor="#fafad2"
-      />
-      <Text> </Text>
-      <Text>
-        {' '}
-        Light Color:
-        {' '}
-        {determineColor}
-      </Text>
-      <Slider
-        minimumValue={0}
-        maximumValue={3}
-        value={initialLightColor}
-        onValueChange={(value) => setLightColor(value)}
-        style={{ width: 160, height: 50 }}
-        onSlidingComplete={(value) => setLightColor(value)}
-        minimumTrackTintColor={stringForInitialColor}
-        maximumTrackTintColor={stringForFinalColor}
-      />
     </View>
   );
 }

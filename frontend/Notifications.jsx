b798bb0d0e4f88f99ab210923ac6e511de530233
vinaynagappala <vinaynagappala@g.ucla.edu>
2021-02-09 20:48:53 -0800

    separating settings pagees into separate files, button text realignment

---
+++ frontend/Notifications.jsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import {
+  Text,
+} from 'react-native';
+import {
+  BoilerplateView,
+} from './Themes';
+
+function Notifications() {
+  return (
+    <BoilerplateView>
+      <Text>Boilerplate for Notifications</Text>
+    </BoilerplateView>
+  );
+}
+
+export default Notifications;
---
+++ frontend/Preferences.jsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import {
+  Text,
+} from 'react-native';
+import {
+  BoilerplateView,
+} from './Themes';
+
+function Preferences() {
+  return (
+    <BoilerplateView>
+      <Text>Boilerplate for Preferences</Text>
+    </BoilerplateView>
+  );
+}
+
+export default Preferences;
